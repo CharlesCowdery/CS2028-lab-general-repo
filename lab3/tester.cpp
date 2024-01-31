22415624f91d3714a5d334e70c6002845f0f0b0c
natwile <156956308+natwile@users.noreply.github.com>
2024-01-31 15:59:36 -0500

    Update tester.cpp

--- lab3/tester.cpp
+++ lab3/tester.cpp
@@ -1,6 +1,35 @@
 #include "heaps.h"
-
+using namespace std
 
 int main(){
+	int littles, lots, heaps;
+	string operation;
+	bool stop = False;
+	while stop != True {
+		cout << "obj1 littles:";
+		cin >> littles;
+		cout << "obj1 lots:";
+		cin >> lots;
+		cout << "obj1 heaps:";
+		cin >> heaps;
+		Measure obj1 = Measure(littles,lots,heaps);
+		cout << "operation to perform(+,-,/,*,==): ";
+		string operation;
+		cout << "obj2 littles:";
+		cin >> littles;
+		cout << "obj2 lots:";
+		cin >> lots;
+		cout << "obj2 heaps:";
+		cin >> heaps;
+		if (operation == "+") {Measure obj3 = obj1+obj2;}
+		else if (operation == "-") {Measure obj3 = obj1-obj2;}
+		else if (operation == "/") {Measure obj3 = obj1/obj2;}
+		else if (operation == "*") {Measure obj3 = obj1*obj2;}
+		else if (operation == "==") {bool obj3 = obj1==obj2;}
+		cout << "results: " << obj3;
+		cout << "Continue? (y/n): ";
+		cin >> operation;
+		if (operation == "n") {stop = True}
+	}
 	return 0;
 }
