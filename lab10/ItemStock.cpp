28697d528a1dbd24817ed1777ca75c687c9ab272
ZuyZuy136 <phamducduyb@gmail.com>
2024-04-08 23:40:36 -0400

    Add ItemStock.cpp

---
+++ lab10/ItemStock.cpp
@@ -0,0 +1,53 @@
+#include "ItemStock.h"
+#include <sstream>
+
+using namespace std;
+
+ItemStock::ItemStock(int Sku, std::string Description, double Price, std::string Uom, double Quantity, double LeadTime) {
+    sku = Sku;
+    description = Description;
+    price = Price;
+    uom = Uom;
+    quantity = Quantity;
+    leadTime = LeadTime;
+}
+
+std::string ItemStock::getPartInfo() {
+    return std::to_string(sku) + " " + description;
+}
+
+std::string ItemStock::getFancyInfo() { //prints out a fancier and more verbose description of an item
+    std::stringstream SS;
+    SS << "----------------" << endl;
+    SS << "| SKU: " << sku << endl;
+    SS << "| Description: " << description << endl;
+    SS << "| Price: $" << getPrice() << endl;
+    SS << "| Quantity: " << quantity << "pcs" << endl;
+    SS << "| Lead time: " << leadTime << endl;
+    SS << "+----------------" << endl;
+    return SS.str();
+}
+
+double ItemStock::getPrice() {
+    return price;
+}
+
+bool ItemStock::inStock() {
+    return quantity > 0;
+}
+
+bool ItemStock::isAvailable(int days) {
+    return (quantity > 0) || (days > leadTime);
+}
+
+bool ItemStock::operator >(ItemStock& right) {
+    return sku > right.sku;
+}
+
+bool ItemStock::operator <(ItemStock& right) {
+    return sku < right.sku;
+}
+
+bool ItemStock::operator ==(ItemStock& right) {
+    return sku == right.sku;
+}
